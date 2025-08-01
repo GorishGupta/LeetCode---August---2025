fa413acc9d610b462e0a81a1ea0f858ae89429ee
Gorish Gupta <gorishgupta50@gmail.com>
2025-08-01 22:12:40 +0530

    Update DS118.java

--- DS118.java
+++ DS118.java
@@ -6,7 +6,7 @@ class DS118 {
 
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
-            
+
             for (int j = 1; j < i; j++) {
                 int val = pascal.get(i - 1).get(j - 1) + pascal.get(i - 1).get(j);
                 row.set(j, val);
@@ -14,5 +14,12 @@ class DS118 {
             pascal.add(row);
         }
         return pascal;
+    }
+
+    public static void main(String[] args) {
+        DS118 solution = new DS118();
+        int numRows = 5;
+        List<List<Integer>> result = solution.generate(numRows);
+        System.out.println(result);
     }
 }
