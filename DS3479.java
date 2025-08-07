1e33e570d485678b9921ca868e30a88d79d3c526
Gorish Gupta <gorishgupta50@gmail.com>
2025-08-07 22:15:28 +0530

    Update DS3479.java

--- DS3479.java
+++ DS3479.java
@@ -1,47 +1,45 @@
 public class DS3479 {
-    class Solution {
-        public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-            int n = baskets.length;
-            int N = 1;
-            while (N <= n)
-                N <<= 1;
-            // Build the segment tree
-            int[] segTree = new int[N << 1];
-            for (int i = 0; i < n; i++)
-                segTree[N + i] = baskets[i];
-            for (int i = N - 1; i > 0; i--)
-                segTree[i] = Math.max(segTree[2 * i], segTree[2 * i + 1]);
-            int count = 0;
-            for (int i = 0; i < n; ++i) {
-                int x = fruits[i];
-                int index = 1; // Start from the root of the segment tree
-                if (segTree[index] < x) {
-                    count++;
-                    continue;
-                }
-                // Query the first valid basket
-                while (index < N) {
-                    if (segTree[index * 2] >= x)
-                        index *= 2;
-                    else
-                        index = index * 2 + 1;
-                }
-                // Mark the found basket as used and update the segment tree
-                segTree[index] = -1;
-                while (index > 1) {
-                    index >>= 1;
-                    segTree[index] = Math.max(segTree[2 * index], segTree[2 * index + 1]);
-                }
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = baskets.length;
+        int N = 1;
+        while (N <= n)
+            N <<= 1;
+        // Build the segment tree
+        int[] segTree = new int[N << 1];
+        for (int i = 0; i < n; i++)
+            segTree[N + i] = baskets[i];
+        for (int i = N - 1; i > 0; i--)
+            segTree[i] = Math.max(segTree[2 * i], segTree[2 * i + 1]);
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            int x = fruits[i];
+            int index = 1; // Start from the root of the segment tree
+            if (segTree[index] < x) {
+                count++;
+                continue;
             }
-            return count;
+            // Query the first valid basket
+            while (index < N) {
+                if (segTree[index * 2] >= x)
+                    index *= 2;
+                else
+                    index = index * 2 + 1;
+            }
+            // Mark the found basket as used and update the segment tree
+            segTree[index] = -1;
+            while (index > 1) {
+                index >>= 1;
+                segTree[index] = Math.max(segTree[2 * index], segTree[2 * index + 1]);
+            }
         }
+        return count;
     }
 
     public static void main(String[] args) {
         DS3479 obj = new DS3479();
-        Solution solution = obj.new Solution();
+
         int fruits[] = { 4, 2, 5 };
         int baskets[] = { 3, 5, 4 };
-        System.out.println(solution.numOfUnplacedFruits(fruits, baskets));
+        System.out.println(obj.numOfUnplacedFruits(fruits, baskets));
     }
 }
