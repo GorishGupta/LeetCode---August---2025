0d8ed44b2dcfd30c365b3b13792311de5035232b
Gorish Gupta <gorishgupta50@gmail.com>
2025-08-12 23:34:38 +0530

    Create DS2787.java

---
+++ DS2787.java
@@ -0,0 +1,24 @@
+public class DS2787 {
+    private static final int MOD = 1_000_000_007;
+
+    public int numberOfWays(int n, int x) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        for (int i = 1; Math.pow(i, x) <= n; i++) {
+            int power = (int) Math.pow(i, x);
+            for (int sum = n; sum >= power; sum--) {
+                dp[sum] = (dp[sum] + dp[sum - power]) % MOD;
+            }
+        }
+        return (int) dp[n];
+    }
+
+    public static void main(String[] args) {
+        DS2787 obj = new DS2787();
+        int n = 10;
+        int x = 2;
+        int result = obj.numberOfWays(n, x);
+        System.out.println("Number of ways to express " + n + " as the sum of unique integers raised to the power of "
+                + x + ": " + result);
+    }
+}
